17196df8d488df093831c910b72ff9616b98087d
Stas <spsereduk3891@gmail.com>
2022-02-08 20:03:51 +0200

    add console app

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
-// console.log(document.body);
-// console.log(document.documentElement);
-console.log(document.body.childNodes);
+const numberOfFilms = prompt('Сколько фильмов Вы уже просмотрели?', '');
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actor: {},
+    genres: [],
+    privat: false
+};
+
+const a = prompt('Один из последних просмотренных фильмов?', ''),
+      b = prompt('На солько его оцените?', ''),
+      c = prompt('Один из последних просмотренных фильмов?', ''),
+      d = prompt('На солько его оцените?', '');
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
